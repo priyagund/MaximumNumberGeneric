e3ada64f28dfb74988698abf02b95f2075fd996b
priyagund <priyagund18@gmail.com>
2019-11-29 14:20:51 +0530

    REFACTOR:changes in variable names in test Maximum

--- src/main/java/com/bridgelabz/Maximumnumber/MaximumNumber.java
+++ src/main/java/com/bridgelabz/Maximumnumber/MaximumNumber.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
 
 public class MaximumNumber<E extends Comparable> {
 
-    public E testMaximum(E... items) {
-        for (int i = 0; i < items.length - 1; i++) {
-            for (int j = 0; j < items.length - i - 1; j++) {
-                if (items[j].compareTo(items[j + 1]) > 0) {
-                    E temp = items[j];
-                    items[j] = items[j + 1];
-                    items[j + 1] = temp;
+    public E testMaximum(E... values) {
+        for (int i = 0; i < values.length - 1; i++) {
+            for (int j = 0; j < values.length - i - 1; j++) {
+                if (values[j].compareTo(values[j + 1]) > 0) {
+                    E temp = values[j];
+                    values[j] = values[j + 1];
+                    values[j + 1] = temp;
                 }
             }
         }
-        printMaximum(items[items.length - 1]);
-        return items[items.length - 1];
+        printMaximum(values[values.length - 1]);
+        return values[values.length - 1];
     }
     public void printMaximum(E maximumNumber) {
         System.out.println("maximum number is " + maximumNumber);
